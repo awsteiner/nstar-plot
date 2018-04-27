2e27edc32748116172b07258fe43caa39f295179
awsteiner <awsteiner@mykolab.com>
2018-04-27 14:39:16 -0400

    Updated license notices.

--- load_crust.py
+++ load_crust.py
@@ -1,7 +1,7 @@
 """
 -------------------------------------------------------------------
 
-Copyright (C) 2015-2016, Andrew W. Steiner
+Copyright (C) 2015-2018, Andrew W. Steiner
 
 This neutron star plot is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License
--- periodic_table.py
+++ periodic_table.py
@@ -1,3 +1,25 @@
+"""
+-------------------------------------------------------------------
+
+Copyright (C) 2015-2018, Andrew W. Steiner
+
+This periodic table plot is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3 of
+the License, or (at your option) any later version.
+
+This periodic table plot is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this periodic table plot. If not, see
+<http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------
+
+"""
 import numpy as np
 import matplotlib.pyplot as plot
 import matplotlib.patches as patches
